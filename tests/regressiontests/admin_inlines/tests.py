982ce362be4f234c2adae2de60745cb062458921
Jonas Obrist <jonas.obrist@divio.ch>
2011-01-21 12:00:33 +0100

    missed an import

--- tests/regressiontests/admin_inlines/tests.py
+++ tests/regressiontests/admin_inlines/tests.py
@@ -7,7 +7,7 @@ from django.test import TestCase
 # local test models
 from models import (Holder, Inner, InnerInline, Holder2, Inner2, Holder3,
     Inner3, Person, OutfitItem, Fashionista, Teacher, Parent, Child,
-    InnerInlineNonExistingTemplate, InnerInlineNoTemplate)
+    InnerInlineNonExistingTemplate, InnerInlineNoTemplate, HolderAdmin)
 
 # local test models
 
